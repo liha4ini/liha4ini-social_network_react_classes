6513d174318f4f14717772a9fb3162694ac491c4
liha4ini <liha4ev.sergey@gmail.com>
2023-09-27 12:25:53 +0300

    minor fix

--- src/components/PostItem/PostItem.tsx
+++ src/components/PostItem/PostItem.tsx
@@ -4,13 +4,20 @@ import s from './postItem.module.css';
 
 type PostItemPropsType = {
     message: string
+    likesCount: number
 }
 
-export const PostItem = (props: PostItemPropsType) => {
+export const PostItem = ({likesCount, message}: PostItemPropsType) => {
     return (
         <div className={s.wrapper}>
-            <img className={s.logo} src="https://avatars.mds.yandex.net/i?id=2a00000179fac26c1049ef26a78eb267c622-4304165-images-thumbs&n=13" alt="logo"/>
-            <span className={s.message}>{props.message}</span>
+            <div className={s.textBlock}>
+                <img className={s.logo} src="https://avatars.mds.yandex.net/i?id=2a00000179fac26c1049ef26a78eb267c622-4304165-images-thumbs&n=13" alt="logo"/>
+                <span className={s.message}>{message}</span>
+            </div>
+            <div className={s.likesBlock}>
+                Likes Count: {likesCount}
+            </div>
+            <span className={s.underLine}></span>
         </div>
     );
 };
--- src/components/Posts/Posts.tsx
+++ src/components/Posts/Posts.tsx
@@ -7,10 +7,10 @@ import s from './posts.module.css';
 export const Posts = () => {
     return (
         <div>
-            <PostItem message={'Are you ok?'} />
-            <PostItem message={'What are ypu doing?'} />
-            <PostItem message={'I am here'} />
-            <PostItem message={'What is new?'} />
+            <PostItem message={'Are you ok?'} likesCount={0} />
+            <PostItem message={'What are ypu doing?'} likesCount={0} />
+            <PostItem message={'I am here'} likesCount={0} />
+            <PostItem message={'What is new?'} likesCount={0} />
         </div>
     );
 };
